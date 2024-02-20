26dfb6b88232e39984efba8da80eb67df1600213
Moon-ByeongHak <nate_id@naver.com>
2024-02-20 16:32:13 +0900

    Initial commit

---
+++ src/main/java/zerobase/maintenance/MaintenanceApplication.java
@@ -0,0 +1,13 @@
+package zerobase.maintenance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaintenanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaintenanceApplication.class, args);
+	}
+
+}
